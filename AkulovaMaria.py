c5cf1dfdad6e88285c13f25e8c97e26e266b0d84
Maria <maru-sia03@mail.ru>
2026-03-16 12:22:15 +0300

    удален комментарий в строке 35

--- AkulovaMaria.py
+++ AkulovaMaria.py
@@ -32,7 +32,7 @@ while try_left > 0:
     print(f'Попыток осталось: {try_left}')
     later = input('Буква: ').lower()
 
-# -------------- проверки --------------
+
     if len(later) != 1 or not later.isalpha():
         print('Нужна одна буква')
         continue
